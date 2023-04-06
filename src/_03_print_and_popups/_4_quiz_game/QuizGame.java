db4100bede82ead727cee71e600ca24ca7139a90
root <root@6d12a5fc887b>
2023-04-06 17:55:50 +0000

    I worked on 6 projects today, and haven't finished working on ScaryMaze

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,35 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+			String question1 = JOptionPane.showInputDialog("What is 3 times 2?");	
 				// 3.  Use an if statement to check if their answer is correct
-				
+			if(question1.equals("6")) {
+			
 				// 4.  if the user's answer was correct, add one to their score 
-		
+				score +=1;
+			}else {
+				score +=-1;
+			}
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
-		
-		
+			String question2 = JOptionPane.showInputDialog("What is 4 times 8?");
+			if(question2.equals("32")) {
+				
+				// 4.  if the user's answer was correct, add one to their score 
+				score +=1;
+			}else {
+				score +=-1;
+			}
 		// After all the questions have been asked, tell the user their final score 
-		
+		JOptionPane.showMessageDialog(null, "your total score is" + score);
 	}
 }
--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -2,35 +2,45 @@ package _03_print_and_popups._5_awesome_or_not;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes everything below
 
-	
+	public static void main (String[] args) {
 		Random ran = new Random();    //This will be used below to make a random number. 
 		
 		// 2. Make a variable that will hold a random whole number
-	
+	int variable = ran.nextInt(4);
 		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
 	
 		// 3. Print your variable to the console
-	
+	System.out.println(variable);
 		// 4. Get the user to enter something that they think is awesome
-	
+	String awesomeThing = JOptionPane.showInputDialog("Enter something that you think is AWESOME!");
 		// 5. If your variable is  0
-	
+		if(variable == 0) {
 			// -- tell the user whatever they entered is awesome!
-	
+			JOptionPane.showMessageDialog(null, "That's Awesome!!!");
+		}
 		// 6. If your variable is  1
-	
+		if (variable == 1) {
 			// -- tell the user whatever they entered is ok.
-	
+			JOptionPane.showMessageDialog(null, "That's ok.");
+		}
 		// 7. If your variable is  2
-	
+		if(variable == 2) {
 			// -- tell the user whatever they entered is boring.
-	
+			JOptionPane.showMessageDialog(null,"That's boring");
+		}
 		// 8. If your variable is  3
-	
+		if(variable == 3) {
+			JOptionPane.showMessageDialog(null, "Nice!3.1415926");
+		}
 			// -- invent your own message to give to the user (be nice).
 
+	}
+	
 }
+		
